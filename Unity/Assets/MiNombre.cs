a7e4e4c1fdb86a7637ba142ca5dc62528e952878
Anzalet <alejandrodamiangonzalez@ceitartessos.com>
2019-10-21 20:22:51 +0200

    subida correcta

---
+++ Unity/Assets/MiNombre.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiNombre : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log ("Alejandro Damian Gonzalez Soriano");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log ("22");
+		
+	}
+}
